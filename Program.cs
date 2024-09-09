6a438ee0860d507c6712b7192dce43f2a20e68ac
vincevince0 <152151247+vincevince0@users.noreply.github.com>
2024-09-09 10:51:25 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,28 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace szinozon
+namespace Színkitaláló
 {
     internal class Program
     {
-        static void answer()
+
+        static int szinvalaszto()
         {
-            Random rnd = new Random();
-            string[] colors = {"blue","red","green","yellow","purple","orange","grey" ,"white"};
-            string[] answers = new string[4];
-            for (int i = 0; i < 4; i++)
+            
+            string[] colors = { "blue", "red", "green", "yellow", "purple", "orange", "grey", "white" };
+            List<string> chosen_colors = new List<string>();          
+
+            int kivalasztott = 0;
+            ConsoleKeyInfo lenyomott;
+
+            do
             {
-                string color = colors[rnd.Next(0,8)];
-                answers[i] = color;
+                Console.Clear();
+                Console.WriteLine("Válassz egy színt: \n");
+
+
+                for (int i = 0; i < colors.Length; i++)
+                {
+                    if (i == kivalasztott)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine($"\t{colors[i]}");
+                }
+
+                lenyomott = Console.ReadKey();
+
+                switch (lenyomott.Key)
+                {
+                    case ConsoleKey.UpArrow: if (kivalasztott > 0) kivalasztott--; break;
+                    case ConsoleKey.DownArrow: if (kivalasztott < colors.Length - 1) kivalasztott++; break;
+                    case ConsoleKey.Enter: if (chosen_colors.Count < 4) chosen_colors.Add(colors[kivalasztott]);  break;
+                }
+            } while (lenyomott.Key != ConsoleKey.Escape);
+
+            Console.WriteLine();
+            for (int i = 0; i < chosen_colors.Count; i++)
+            {
+                Console.WriteLine(chosen_colors[i]);
             }
-            /*for (int i = 0;i < 4; i++)
-            {
-                Console.WriteLine(answers[i]);
-            }*/
+            return kivalasztott;
         }
+
+
+    
+
         static void Main(string[] args)
         {
-            answer();
+
+            szinvalaszto();
+                
+
         }
     }
 }
