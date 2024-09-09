a6017e6d8e4a12dfc28aa8e17dff069458d101e8
szabobarni <152151212+szabobarni@users.noreply.github.com>
2024-09-09 11:17:23 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,17 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Színkitaláló
+namespace szinozon
 {
     internal class Program
     {
-
+        public string[] colors = { "blue", "red", "green", "yellow", "purple", "orange", "grey", "white" };
+        static string[] answer(string[] colors)
+        {
+            Random rnd = new Random();
+            string[] answers = new string[4];
+            for (int i = 0; i < 4; i++)
+            {
+                string color = colors[rnd.Next(0,8)];
+                answers[i] = color;
+            }
+            /*for (int i = 0;i < 4; i++)
+            {
+                Console.WriteLine(answers[i]);
+            }*/
+            return answers;
+        }
         static int szinvalaszto()
         {
-            
+            List<string> chosen_colors = new List<string>();
             string[] colors = { "blue", "red", "green", "yellow", "purple", "orange", "grey", "white" };
-            List<string> chosen_colors = new List<string>();          
-
             int kivalasztott = 0;
             ConsoleKeyInfo lenyomott;
 
@@ -43,7 +56,9 @@ namespace Színkitaláló
                 {
                     case ConsoleKey.UpArrow: if (kivalasztott > 0) kivalasztott--; break;
                     case ConsoleKey.DownArrow: if (kivalasztott < colors.Length - 1) kivalasztott++; break;
-                    case ConsoleKey.Enter: if (chosen_colors.Count < 4) chosen_colors.Add(colors[kivalasztott]);  break;
+                    case ConsoleKey.Enter: if (chosen_colors.Count < 4) chosen_colors.Add(colors[kivalasztott]); break;
+                    case ConsoleKey.L:
+                        check(colors); break;
                 }
             } while (lenyomott.Key != ConsoleKey.Escape);
 
@@ -54,16 +69,20 @@ namespace Színkitaláló
             }
             return kivalasztott;
         }
-
-
-    
+        static void check(string[] colors)
+        {
+            string[] answers = answer(colors);
+            for (int i = 0; i < answers.Length; i++)
+            {
+                Console.WriteLine(answers[i]);
+            }
+        }
 
         static void Main(string[] args)
         {
-
+            // answer();
             szinvalaszto();
-                
-
+            
         }
     }
 }
