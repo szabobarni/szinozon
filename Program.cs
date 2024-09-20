430448b3881046c114417b3794bdab2a63601bee
szabobarni <152151212+szabobarni@users.noreply.github.com>
2024-09-20 08:34:18 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,43 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace szinozon
+namespace Színkitaláló
 {
     internal class Program
     {
-        public string[] colors = { "blue", "red", "green", "yellow", "purple", "orange", "grey", "white" };
-        static string[] answer(string[] colors)
+
+        static string[] colors = { "blue", "red", "green", "yellow", "purple", "orange", "grey", "white" };
+        //static List<string> chosen_colors = new List<string>();
+
+        static List<string> start()
         {
+            Console.Clear();
             Random rnd = new Random();
-            string[] answers = new string[4];
-            for (int i = 0; i < 4; i++)
-            {
-                string color = colors[rnd.Next(0,8)];
-                answers[i] = color;
-            }
-            /*for (int i = 0;i < 4; i++)
-            {
-                Console.WriteLine(answers[i]);
-            }*/
-            return answers;
-        }
-        static int szinvalaszto()
-        {
             List<string> chosen_colors = new List<string>();
-            string[] colors = { "blue", "red", "green", "yellow", "purple", "orange", "grey", "white" };
+
+            Console.Write("Összes szín: ");
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Console.Write(colors[i] + "; ");
+            }
+            Console.WriteLine();
+            Console.Write("Válassz hány színt kelljen kitalálni: ");
+            int hany = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < hany; i++)
+            {
+                chosen_colors.Add(colors[rnd.Next(0, 8)]);
+            }
+            return chosen_colors;
+        }
+
+        static void guesser()
+        {
+            List<string> chosen_colors = start();
+            for (int i = 0; i < chosen_colors.Count; i++)
+            {
+                Console.WriteLine(chosen_colors[i] + " ");
+            }
+
+            List<string> guessed_colors = new List<string>();
+
+            bool win = false;
+            int pontok = 0;
+            while (win != true)
+            {
+                int k = 0;
+                while (k != chosen_colors.Count)
+                {
+
+                    Console.Write($"Add meg a(z) {k + 1}. színt: ");
+                    string num = Console.ReadLine();
+                    guessed_colors.Add(num);
+                    k++;
+                }
+
+                for (int i = 0; i < chosen_colors.Count; i++)
+                {
+                    if (chosen_colors.Contains(guessed_colors[i]))
+                    {
+                        if (guessed_colors[i] == chosen_colors[i])
+                        {
+                            Console.WriteLine($"{i + 1}. színt eltaláltad");
+                            pontok++;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{i+1}. szín benne van csak rossz helyen");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{i + 1}. szín nem talált");
+                    }
+                }
+                if ( pontok == chosen_colors.Count )
+                {
+                    win = true;
+                    ujjatek();
+                }
+                guessed_colors.Clear();
+                chosen_colors.Clear();
+            }
+        }
+        static void ujjatek()
+        {
+            Console.Clear();
+            
             int kivalasztott = 0;
             ConsoleKeyInfo lenyomott;
+            string[] opcio = { "Új játék", "Kilépés" };
 
             do
             {
                 Console.Clear();
-                Console.WriteLine("Válassz egy színt: \n");
+                Console.WriteLine("Nyertél");
+                Console.WriteLine("Válassz az alábbiak kozül: \n");
 
 
-                for (int i = 0; i < colors.Length; i++)
+                for (int i = 0; i < opcio.Length; i++)
                 {
                     if (i == kivalasztott)
                     {
@@ -47,7 +112,7 @@ namespace szinozon
                     {
                         Console.ForegroundColor = ConsoleColor.White;
                     }
-                    Console.WriteLine($"\t{colors[i]}");
+                    Console.WriteLine($"\t{opcio[i]}");
                 }
 
                 lenyomott = Console.ReadKey();
@@ -55,34 +120,25 @@ namespace szinozon
                 switch (lenyomott.Key)
                 {
                     case ConsoleKey.UpArrow: if (kivalasztott > 0) kivalasztott--; break;
-                    case ConsoleKey.DownArrow: if (kivalasztott < colors.Length - 1) kivalasztott++; break;
-                    case ConsoleKey.Enter: if (chosen_colors.Count < 4) chosen_colors.Add(colors[kivalasztott]); break;
-                    case ConsoleKey.L:
-                        check(colors); break;
+                    case ConsoleKey.DownArrow: if (kivalasztott < opcio.Length - 1) kivalasztott++; break;
                 }
-            } while (lenyomott.Key != ConsoleKey.Escape);
-
-            Console.WriteLine();
-            for (int i = 0; i < chosen_colors.Count; i++)
+            } while (lenyomott.Key != ConsoleKey.Enter);
+            if (kivalasztott == 0)
             {
-                Console.WriteLine(chosen_colors[i]);
+                start();
+                guesser();
             }
-            return kivalasztott;
-        }
-        static void check(string[] colors)
-        {
-            string[] answers = answer(colors);
-            for (int i = 0; i < answers.Length; i++)
+            else
             {
-                Console.WriteLine(answers[i]);
+                Console.Clear();
+                Console.WriteLine("Viszlát");
             }
         }
 
         static void Main(string[] args)
         {
-            // answer();
-            szinvalaszto();
-            
+            start();
+            guesser();
         }
     }
 }
